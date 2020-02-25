1b38977c0836292cd347e99392b004558f81e0a4
azat baran <azat_2600@hotmail.com>
2020-02-25 10:29:55 +0100

    lavet Header model

---
+++ WebsiteCloudProBackOfficeApi/Models/Header.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebsiteCloudProBackOfficeApi.Models
+{
+    public class Header {
+
+        public static int nextid = 0;
+        public int ID { get; set; }
+        public string Titel { get; set; }
+        public string Description { get; set; }
+        public Header()
+        {
+            ID = nextid++;
+        }
+
+        public Header(string titel, string description)
+        {
+            ID = nextid++;
+            Titel = titel;
+            Description = description;
+        }
+    }
+}
